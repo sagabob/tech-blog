aacb25589742aaad7a7af7dab94f15b5ee75dc18
DESK-TDP-01\bobpham <bobpham.tdp@gmail.com>
2022-02-05 22:40:02 +1100

    work in progress

--- src/pages/About.js
+++ src/pages/About.js
@@ -58,60 +58,98 @@ const About = () => {
                     </div>
                     <div className="section-content">
                       <div className="experience-wrapper">
-                        <div className="experience-item">
-                          <img src="https://via.placeholder.com/150x150" alt="" />
-                          <div className="meta">
-                            <span className="dark-inverted">Hexagon Safety & Infrastructure</span>
-                            <span >
-                              <span>Full-time &nbsp;</span>
-                              <span>&#8213;</span>
-                              <span>&nbsp; 7 yrs 5 mos</span>
-                            </span>
+                        <div className="experience-item-wrapper">
+                          <div className="experience-item">
+                            <img src="https://via.placeholder.com/150x150" alt="" />
+                            <div className="meta">
+                              <span className="dark-inverted">.id (informed decisions)</span>
+                              <span >
+                                <span>Full-time &nbsp;</span>
+                                <span>&#8213;</span>
+                                <span>&nbsp; 1 yrs 3 mos</span>
+                              </span>
+                            </div>
                           </div>
-                        </div>
-                        <div className="timeline">
-                          <div className="outer">
-                            <div className="timeline-card">
-                              <h5 className="subtitle is-6">Senior Software Developer (Public Safety)</h5>
-                              <div className="timeperiod">
-                                <span>
-                                  <span>Jan 2019</span>
-                                  <span>&#8213;</span>
-                                  <span>Nov 2020</span>
-                                  <span>(1 yr 11 mos)</span>
-                                </span>
-                              </div>
-                              <div className="location-description">
-                                <span>Melbourne, Australia</span>
-                              </div>
-                              <div className="key-description">
-                                <ul >
-                                  <li>Handle software release management process, work as release manager.</li>
-                                  <li>Work as Devops manager, migrate TFS code to Azure DevOps, setting up build/release pipelines.</li>
-                                  <li>Development & coordinate with other developers, testers, implementers and release manager.</li>
-                                </ul>
+                          <div className="timeline">
+                            <div className="outer">
+                              <div className="timeline-card">
+                                <h5 className="subtitle is-6">Senior Software Developer (Public Safety)</h5>
+                                <div className="timeperiod">
+                                  <span>
+                                    <span>Dec 2020</span>
+                                    <span>&#8213;</span>
+                                    <span>Present</span>
+                                  </span>
+                                </div>
+                                <div className="location-description">
+                                  <span>Melbourne, Australia</span>
+                                </div>
+                                <div className="key-description">
+                                  <ul >
+                                    <li>Handle all technical work (design/architechture/develop/deploy) on all 3 business applications</li>
+                                    <li>Maintain the infrastructure</li>
+                                  </ul>
+                                </div>
                               </div>
                             </div>
+                          </div>
+                        </div>
+                        <div className="experience-item-wrapper">
+                          <div className="experience-item">
+                            <img src="https://via.placeholder.com/150x150" alt="" />
+                            <div className="meta">
+                              <span className="dark-inverted">Hexagon Safety & Infrastructure</span>
+                              <span >
+                                <span>Full-time &nbsp;</span>
+                                <span>&#8213;</span>
+                                <span>&nbsp; 7 yrs 5 mos</span>
+                              </span>
+                            </div>
+                          </div>
+                          <div className="timeline">
+                            <div className="outer">
+                              <div className="timeline-card">
+                                <h5 className="subtitle is-6">Senior Software Developer (Public Safety)</h5>
+                                <div className="timeperiod">
+                                  <span>
+                                    <span>Jan 2019</span>
+                                    <span>&#8213;</span>
+                                    <span>Nov 2020</span>
+                                    <span>(1 yr 11 mos)</span>
+                                  </span>
+                                </div>
+                                <div className="location-description">
+                                  <span>Melbourne, Australia</span>
+                                </div>
+                                <div className="key-description">
+                                  <ul >
+                                    <li>Handle software release management process, work as release manager.</li>
+                                    <li>Work as Devops manager, migrate TFS code to Azure DevOps, setting up build/release pipelines.</li>
+                                    <li>Development & coordinate with other developers, testers, implementers and release manager.</li>
+                                  </ul>
+                                </div>
+                              </div>
 
-                            <div className="timeline-card">
-                              <h5 className="subtitle is-6">GIS Analyst Consultant/Senior Developer (Geospatial)</h5>
-                              <div className="timeperiod">
-                                <span>
-                                  <span>Jul 2013</span>
-                                  <span>&#8213;</span>
-                                  <span>Dec 2018</span>
-                                  <span>(5 yrs 6 mos)</span>
-                                </span>
-                              </div>
-                              <div className="location-description">
-                                <span>Auckland, New Zealand</span>
-                              </div>
-                              <div className="key-description">
-                                <ul >
-                                  <li>Consult/support clients on a wide range of Hexagon geospatial software.</li>
-                                  <li>Involve in the whole software development cycle, design/develop/test/deploy software on the client environments, then provide support.</li>
-                                  <li>Coordinate & provide technical advises for overseas team to do development.</li>
-                                </ul>
+                              <div className="timeline-card">
+                                <h5 className="subtitle is-6">GIS Analyst Consultant/Senior Developer (Geospatial)</h5>
+                                <div className="timeperiod">
+                                  <span>
+                                    <span>Jul 2013</span>
+                                    <span>&#8213;</span>
+                                    <span>Dec 2018</span>
+                                    <span>(5 yrs 6 mos)</span>
+                                  </span>
+                                </div>
+                                <div className="location-description">
+                                  <span>Auckland, New Zealand</span>
+                                </div>
+                                <div className="key-description">
+                                  <ul >
+                                    <li>Consult/support clients on a wide range of Hexagon geospatial software.</li>
+                                    <li>Involve in the whole software development cycle, design/develop/test/deploy software on the client environments, then provide support.</li>
+                                    <li>Coordinate & provide technical advises for overseas team to do development.</li>
+                                  </ul>
+                                </div>
                               </div>
                             </div>
                           </div>
