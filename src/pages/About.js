ad52cc272a9c67a6de73233f1787455786d82136
DESK-TDP-01\bobpham <bobpham.tdp@gmail.com>
2022-02-05 21:31:01 +1100

    work in progress

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCodeBranch, fa } from '@fortawesome/free-solid-svg-icons'
+import { faCodeBranch, faUsers } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub, faCodepen } from '@fortawesome/free-brands-svg-icons'
 import { selectedLightColor } from '../util/color'
 
@@ -16,11 +16,16 @@ const About = () => {
               <img className="badge" src="https://via.placeholder.com/150x150" alt="" />
             </div>
             <h3 className="title is-4 is-narrow is-thin no-bottom-margin">Bob Pham</h3>
-            <p className="light-text">
+            <p className="bio-description">
               Hey everyone, I'm a full-stack developer. I’m interested in software architechture, frontend frameworks<br />
               and containers. I’m currently learning python and machine learning.
             </p>
             <div className="profile-stats">
+              <div class="profile-stat">
+                <FontAwesomeIcon icon={faUsers} color={selectedLightColor} />
+                <span>50+ Relations</span>
+              </div>
+              <div class="separator"></div>
               <div className="profile-stat">
                 <FontAwesomeIcon icon={faCodeBranch} color={selectedLightColor} />
                 <span>78 Projects</span>
@@ -56,28 +61,60 @@ const About = () => {
                         <div className="experience-item">
                           <img src="https://via.placeholder.com/150x150" alt="" />
                           <div className="meta">
-                            <span className="dark-inverted">.Id (informed decisions)</span>
-                            <span>
-                              <span>Dec 2020 &nbsp;</span>
+                            <span className="dark-inverted">Hexagon Safety & Infrastructure</span>
+                            <span >
+                              <span>Full-time &nbsp;</span>
                               <span>&#8213;</span>
-                              <span>&nbsp; Present</span>
+                              <span>&nbsp; 7 yrs 5 mos</span>
                             </span>
                           </div>
                         </div>
-                        <div>
-                          <ul className="steps is-vertical">
-                            <li>
-                              <span className="steps-marker"></span>
-                              <div className="steps-content">
-                                <p className="is-size-6">Senior Software Developer (Public Safety)</p>
+                        <div className="timeline">
+                          <div className="outer">
+                            <div className="timeline-card">
+                              <h5 className="subtitle is-6">Senior Software Developer (Public Safety)</h5>
+                              <div className="timeperiod">
                                 <span>
-                                  <span>Dec 2020 &nbsp;</span>
+                                  <span>Jan 2019</span>
                                   <span>&#8213;</span>
-                                  <span>&nbsp; Present</span>
+                                  <span>Nov 2020</span>
+                                  <span>(1 yr 11 mos)</span>
                                 </span>
                               </div>
-                            </li>
-                          </ul>
+                              <div className="location-description">
+                                <span>Melbourne, Australia</span>
+                              </div>
+                              <div className="key-description">
+                                <ul >
+                                  <li>Handle software release management process, work as release manager.</li>
+                                  <li>Work as Devops manager, migrate TFS code to Azure DevOps, setting up build/release pipelines.</li>
+                                  <li>Development & coordinate with other developers, testers, implementers and release manager.</li>
+                                </ul>
+                              </div>
+                            </div>
+
+                            <div className="timeline-card">
+                              <h5 className="subtitle is-6">GIS Analyst Consultant/Senior Developer (Geospatial)</h5>
+                              <div className="timeperiod">
+                                <span>
+                                  <span>Jul 2013</span>
+                                  <span>&#8213;</span>
+                                  <span>Dec 2018</span>
+                                  <span>(5 yrs 6 mos)</span>
+                                </span>
+                              </div>
+                              <div className="location-description">
+                                <span>Auckland, New Zealand</span>
+                              </div>
+                              <div className="key-description">
+                                <ul >
+                                  <li>Consult/support clients on a wide range of Hexagon geospatial software.</li>
+                                  <li>Involve in the whole software development cycle, design/develop/test/deploy software on the client environments, then provide support.</li>
+                                  <li>Coordinate & provide technical advises for overseas team to do development.</li>
+                                </ul>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -87,7 +124,7 @@ const About = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div >
     </>
   )
 }
